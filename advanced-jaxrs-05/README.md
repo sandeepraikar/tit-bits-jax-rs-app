1f5f44906ae2c3b462a1bb56d62d6fe596e92e0b
sandeepraikar <snraikar@gmail.com>
2016-04-23 12:19:27 -0500

    added implementation for custom media types and messagebodywriter for date type

---
+++ advanced-jaxrs-05/README.md
@@ -0,0 +1,4 @@
+# my-test-app-jaxrs
+
+* Implemented MessageBodyWriter for Date type - GET Method
+* Implementation of custom media types - "text/shortdate"
---
+++ advanced-jaxrs-05/src/main/java/edu/sraikar/ShortDateMessageBodyWriter.java
@@ -0,0 +1,45 @@
+package edu.sraikar;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Date;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Produces("text/shortdate")
+public class ShortDateMessageBodyWriter implements MessageBodyWriter<Date>{
+
+	@Override
+	public long getSize(Date arg0, Class<?> arg1, Type arg2, Annotation[] arg3,
+			MediaType arg4) {
+		// This method is deprecated!!! So returning -1 straight away
+		return -1;
+	}
+
+	@Override
+	public boolean isWriteable(Class<?> rawType, Type arg1, Annotation[] arg2,
+			MediaType arg3) {		
+		return Date.class.isAssignableFrom(rawType);
+	}
+
+	@Override
+	public void writeTo(Date date, Class<?> rawType,
+			Type type1,
+			Annotation[] antns,
+			MediaType mt,
+			MultivaluedMap<String, Object> mm,
+			OutputStream out) throws IOException, WebApplicationException {
+		String shortDate = date.getDate() +"-"+date.getMonth()+"-"+date.getYear();
+		out.write(shortDate.getBytes());
+		
+	}
+
+}
